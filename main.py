55431c0789a9147ce90a0ff7d98b192ae878f475
piter_1316 <piter_1316@wp.pl>
2018-04-24 15:59:10 +0200

    COS jest

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import pymysql
 
 database = pymysql.connect('b2b.int-technics.pl', 'b2b_roboczy', 'b2b_roboczy', 'b2b_robocza')
 cursor = database.cursor()
+tables = ['__OMRON_Cennik','__PILZ_Cennik_copy','__ABB_Cennik','__ALLEN-BRADLEY_Cennik']
+
+
+
 
 
 def clickSearchButton():
-    global product
+
 
     kodTowaru = inputField_1.get()
 
@@ -17,48 +21,69 @@ def clickSearchButton():
     results = cursor.fetchall()
     try:
         for row in results:
-            kod = row[0]
+
             kontrahent = row[1]
+            kontrahentCennik = row[2]
             cenaKatalogowa = row[7]
             walutaKatalogowa = row[8]
 
             product= []
-            product.append(kod)
             product.append(kontrahent)
+            product.append(kontrahentCennik)
             product.append(cenaKatalogowa)
             product.append(walutaKatalogowa)
             print(product)
             outputField.insert(tk.INSERT,product)
-            outputField.insert(tk.END, "\n")
+            outputField.insert(tk.END, '\n')
+
+
 
     #kontrola
     except:
         print('NIE ZNALEZIONO')
+
+
 def clickClearResults():
     outputField.delete('1.0', tk.END)
+
+def getTables(tab):
+    for i in range (len(tab)):
+        return(tables[i])
+
+
 window_1 = tk.Tk()
 window_1.maxsize(width=1280, height=720)
 window_1.minsize(width=1280, height=720)
+window_1.title('CENNIKI')
 
 searchButton = tk.Button(window_1, text='WYSZUKAJ', command=clickSearchButton)
-# searchButton.place(x=10, y=10)
 searchButton.pack(side=tk.TOP)
 
-clearButton = tk.Button(window_1, text='WYCZYŚć WYSZYKIWANE', command=clickClearResults)
-# searchButton.place(x=10, y=10)
+clearButton = tk.Button(window_1, text='WYCZYŚć WYSZUKIWANE', command=clickClearResults)
 clearButton.pack(side=tk.TOP)
 
 searchTextField = tk.Label(window_1, text='Podaj kodTowaru: ')
-searchTextField.place(x=10, y=10)
+searchTextField.pack(side=tk.TOP)
+
 inputField_1 = tk.Entry(window_1)
 inputField_1.place(x=150, y=10)
+inputField_1.pack(side=tk.TOP)
 inputField_1.focus()
 
 outputField = tk.Text()
 outputField.place(x=150, y=150)
+outputField.pack(side=tk.TOP)
+
+var = tk.StringVar(window_1)
+default = tables[0]
+
+tables_list = tk.OptionMenu(window_1,var,*tables)
 
 
 
+tables_list.pack()
+tables_list.place(x=20,y=20)
 
-
+print(getTables(tables))
 window_1.mainloop()
+
