c73c81dbe493c788e8c95d1eae99f9d04a733815
piter_1316 <piter_1316@wp.pl>
2018-04-27 12:44:01 +0200

    Więcej funkcji, dodane powiadomienia

--- main.py
+++ main.py
@@ -1,126 +1,152 @@
 import tkinter as tk
 from tkinter import messagebox
 import tkinter.ttk
-
 import pymysql
 
-window_1 = tk.Tk()
-window_1.maxsize(width=1280, height=720)
-window_1.minsize(width=1280, height=720)
-window_1.title('CENNIKI')
-resultTable = tkinter.ttk.Treeview(window_1)
+def connectToDatabase(url,user,password,dataBaseName):
+    database = pymysql.connect(url,user,password,dataBaseName)
+    global cursor
+    cursor= database.cursor()
+    return database
+def defineResultTable(frame,mode,columns):
+    table = tkinter.ttk.Treeview(frame,selectmode=mode,columns=columns)
+    return table
 
+def setResultTable(resultTable):
+    resultTable.column("kontrahent", width=150)
+    resultTable.column("cennik", width=80)
+    resultTable.column("cenaKoncowa", width=100)
+    resultTable.column("cenaKatalogowa", width=100)
+    resultTable.column("Rabat", width=60)
+    resultTable.column("cenaKoncowaEUR", width=110)
+    resultTable.column("zDnia", width=80)
+    resultTable.heading("kontrahent", text="kontrahent")
+    resultTable.heading("cennik", text="cennik")
+    resultTable.heading("cenaKoncowa", text="cenaKoncowa")
+    resultTable.heading("cenaKatalogowa", text="cenaKatalogowa")
+    resultTable.heading("Rabat", text="Rabat")
+    resultTable.heading("cenaKoncowaEUR", text="cenaKoncowaEUR")
+    resultTable.heading("zDnia", text="zDnia")
+    resultTable['show'] = 'headings'
+    return resultTable
 
-database = pymysql.connect('b2b.int-technics.pl', 'b2b_roboczy', 'b2b_roboczy', 'b2b_robocza')
-cursor = database.cursor()
+def showTable(resultTable,row,column):
+    resultTable.grid(row=row, column=column)
+
+def prepareSqlSelectQuery():
+    kodTowaru = str(inputField_1.get())
+    kodTowaru = kodTowaru.replace('"', '')
+    kodTowaru = kodTowaru.replace("'", "")
+    kodTowaru = kodTowaru.replace("&", "")
+    kodTowaru = kodTowaru.replace("<!--", "")
+    kodTowaru = kodTowaru.replace("-->", "")
+    kodTowaru = kodTowaru.replace(">", "")
+    kodTowaru = kodTowaru.replace("<", "")
+    kodTowaru = kodTowaru.replace("$", "")
+    kodTowaru = kodTowaru.replace("\r", "")
+    kodTowaru = kodTowaru.replace("!", "")
+
+    sql = "SELECT * FROM " + "`" + optionMenuValue.get() + "`" + " WHERE kodTowaru = '{}'".format(kodTowaru)
+    return sql
 
 def clickSearchButton():
 
-    kodTowaru = inputField_1.get()
-    sql = "SELECT * FROM "+"`"+optionMenuValue.get()+"`"+ " WHERE kodTowaru = '{}'".format(kodTowaru)
+    try:
+        cursor.execute(prepareSqlSelectQuery())
 
-    cursor.execute(sql)
+        results = cursor.fetchall()
 
-    results = cursor.fetchall()
-    outputField.config(state=tk.NORMAL)
+        if len(results)>0:
 
-    if len(results)>0:
+            try:
+                for row in results:
 
-        try:
-            for row in results:
+                    kontrahent = row[1]
+                    kontrahentCennik = row[2]
+                    cenaKoncowa = row[7]
+                    cenaKatalogowaEUR = row[8]
+                    rabat= row[10]
+                    zDnia = row[11]
+                    cenaKonEUR = row[9]
 
-                kontrahent = row[1]
-                kontrahentCennik = row[2]
-                cenaKoncowa = row[7]
-                cenaKatalogowaEUR = row[8]
-                rabat= row[10]
-                zDnia = row[11]
-                cenaKonEUR = row[9]
+                    cenaKatalogowaEUR = round(cenaKatalogowaEUR,2)
+                    cenaKonEUR = round(cenaKonEUR,2)
+                    rabat = round(rabat,2)
 
-                cenaKatalogowaEUR = round(cenaKatalogowaEUR,2)
-                cenaKonEUR = round(cenaKonEUR,2)
-                rabat = round(rabat,2)
+                    product= []
+                    product.append(kontrahent)
+                    product.append(kontrahentCennik)
+                    product.append(cenaKoncowa)
+                    product.append(cenaKatalogowaEUR)
+                    product.append(rabat)
+                    product.append(zDnia)
+                    product.append(cenaKonEUR)
+                    resultTable.insert("", 1, values=(kontrahent, kontrahentCennik, cenaKoncowa, cenaKatalogowaEUR, rabat, cenaKonEUR, zDnia))
 
-                product= []
-                product.append(kontrahent)
-                product.append(kontrahentCennik)
-                product.append(cenaKoncowa)
-                product.append(cenaKatalogowaEUR)
-                product.append(rabat)
-                product.append(zDnia)
-                product.append(cenaKonEUR)
+            except:
+                messagebox.showinfo("Błąd POBIERANIA DANYCH")
 
-                outputField.insert(tk.INSERT,product)
-                outputField.insert(tk.END, '\n')
-                outputField.insert(tk.END, '______________________________________________________________________\n')
-
-                resultTable.insert("", 0, values=(kontrahent, kontrahentCennik, cenaKoncowa, cenaKatalogowaEUR, rabat, cenaKonEUR, zDnia))
-                print("dodano do wyswietlenia")
-
-        except:
-            print('NIE ZNALEZIONO')
-    else:
-        messagebox.showinfo("NIE ZNALEZIONO", "BRAK KODU W BAZIE")
-        print("nic")
-
-    outputField.config(state=tk.DISABLED)
-
-
-
+                print('NIE ZNALEZIONO')
+        else:
+            messagebox.showinfo("NIE ZNALEZIONO", "BRAK KODU W BAZIE")
+    except:
+        messagebox.showinfo("Błąd","Podana Baza danych nie istnieje!")
 
 def clickClearResults():
-    outputField.config(state=tk.NORMAL)
-    outputField.delete('1.0', tk.END)
     for i in resultTable.get_children():
         resultTable.delete(i)
-
 
 def getTablesList():
     cursor = database.cursor()
     cursor.execute("SHOW TABLES in b2b_robocza LIKE 'zestaw%' ")
-
     tables = cursor.fetchall()
     getTables_list = []
+
     for (table_name,) in tables:
         getTables_list.append(table_name)
-
     return getTables_list
 
-resultTable["columns"] = ("kontrahent", "cennik", "cenaKoncowa", "cenaKatalogowa", "Rabat", "cenaKoncowaEUR", "zDnia")
-resultTable.column("kontrahent", width=100)
-resultTable.column("cennik", width=100)
-resultTable.column("cenaKoncowa", width=100)
-resultTable.column("cenaKatalogowa", width=100)
-resultTable.column("Rabat", width=100)
-resultTable.column("cenaKoncowaEUR", width=100)
-resultTable.column("zDnia", width=100)
-resultTable.heading("kontrahent", text="kontrahent")
-resultTable.heading("cennik", text="cennik")
-resultTable.heading("cenaKoncowa", text="cenaKoncowa")
-resultTable.heading("cenaKatalogowa", text="cenaKatalogowa")
-resultTable.heading("Rabat", text="Rabat")
-resultTable.heading("cenaKoncowaEUR", text="cenaKoncowaEUR")
-resultTable.heading("zDnia", text="zDnia")
+def setButton(frame,text,command,row,column):
+    button = tk.Button(frame, text=text, command=command)
+    button.grid(row=row,column=column)
+    return button
+
+def setLabel(frame,text,row,column):
+    label =  tk.Label(frame, text=text)
+    label.grid(row=row,column=column)
+    return label
+
+def setWindow(title, width, height):
+    window = tk.Tk()
+    window.maxsize(width=width, height=height)
+    window.minsize(width=width, height=height)
+    window.title(title)
+    return window
+
+def setInputField(frame,row,column):
+    field = tk.Entry(frame)
+    field.grid(row=row, column=column)
+    return field
 
 
-resultTable.grid(row=0, column=0)
+#main
+window_1 = setWindow('CENNIK',720,480)
 
-searchButton = tk.Button(window_1, text='WYSZUKAJ', command=clickSearchButton)
-searchButton.grid(row=5, column=0)
+database = connectToDatabase('b2b.int-technics.pl', 'b2b_roboczy', 'b2b_roboczy', 'b2b_robocza')
 
-clearButton = tk.Button(window_1, text='WYCZYŚć WYSZUKIWANE', command=clickClearResults)
-clearButton.grid(row=7, column=0)
+resultTable = defineResultTable(window_1,"extended",("kontrahent", "cennik", "cenaKoncowa", "cenaKatalogowa", "Rabat", "cenaKoncowaEUR", "zDnia"))
 
-searchTextField = tk.Label(window_1, text='Podaj kod Towaru: ')
-searchTextField.grid(row=1,column=0)
+setResultTable(resultTable)
+showTable(resultTable,0,0)
 
-inputField_1 = tk.Entry(window_1)
-inputField_1.grid(row=2, column=0)
+searchButton = setButton(window_1,'WYSZUKAJ',clickSearchButton,5,0)
+
+clearButton = setButton(window_1,"WYCZYŚć WYSZUKIWANIE",clickClearResults,6,0)
+
+searchTextLabel = setLabel(window_1, 'Podaj kod Towaru: ', 1, 0)
+
+inputField_1 = setInputField(window_1,2,0) #tk.Entry(window_1)
 inputField_1.focus()
-
-outputField = tk.Text()
-outputField.grid(row=8, column=0)
-outputField.config(state=tk.DISABLED)
 
 scrollbar = tk.Scrollbar(window_1, command=resultTable.yview)
 scrollbar.grid(row=0, column=2, sticky=tk.N)
