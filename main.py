ffe2174cf81143d122a9737b5cca85b1e2b5f49e
piter_1316 <piter_1316@wp.pl>
2018-04-30 10:42:54 +0200

    Export do xlsx z wyborem ścieżki, refaktoryzacja nazw

--- main.py
+++ main.py
@@ -1,75 +1,87 @@
 import tkinter as tk
-import xlsxwriter
-from tkinter import messagebox
 import tkinter.ttk
+from tkinter import messagebox, filedialog
+from typing import List, Any
 import pymysql
-#global dabase
+import xlsxwriter
+
+# global dabase
 data = []
 
 
-def connectToDatabase(url,user,password,dataBaseName):
-    database = pymysql.connect(url,user,password,dataBaseName)
+def connect_to_database(url, user, password, data_base_name):
+    database = pymysql.connect(url, user, password, data_base_name)
     global cursor
-    cursor= database.cursor()
+    cursor = database.cursor()
     return database
-def disconectFromDatabase(database):
+
+
+def disconect_from_database(database):
     database.close()
 
-def defineResultTable(frame,mode,columns):
-    table = tkinter.ttk.Treeview(frame,selectmode=mode,columns=columns)
+
+def define_result_table(frame, mode, columns):
+    table = tkinter.ttk.Treeview(frame, selectmode=mode, columns=columns)
     return table
 
-def setResultTable(resultTable):
-    resultTable.heading("kodTowaru",text="kod")
-    resultTable.column("kodTowaru", width=130)
 
-    resultTable.heading("kontrahent", text="kontrahent")
-    resultTable.column("kontrahent", width=150)
+def set_result_table(result_table):
+    result_table.heading("kodTowaru", text="kodTowaru")
+    result_table.column("kodTowaru", width=130)
 
-    resultTable.heading("cennik", text="cennik")
-    resultTable.column("cennik", width=80)
+    result_table.heading("kontrahent", text="kontrahent")
+    result_table.column("kontrahent", width=150)
 
-    resultTable.heading("cenaKoncowa", text="cenaKoncowa[wal]")
-    resultTable.column("cenaKoncowa", width=110)
+    result_table.heading("cennik", text="cennik")
+    result_table.column("cennik", width=80)
 
-    resultTable.heading("cenaKatalogowa", text="cenaKatalogowa[€]")
-    resultTable.column("cenaKatalogowa", width=110)
+    result_table.heading("cenaKoncowa", text="cenaKoncowa[wal]")
+    result_table.column("cenaKoncowa", width=110)
 
-    resultTable.heading("Rabat", text="Rabat")
-    resultTable.column("Rabat", width=50)
+    result_table.heading("cenaKatalogowa", text="cenaKatalogowa[€]")
+    result_table.column("cenaKatalogowa", width=110)
 
-    resultTable.heading("cenaKoncowaEUR", text="cenaKoncowa[€]")
-    resultTable.column("cenaKoncowaEUR", width=110)
+    result_table.heading("Rabat", text="Rabat")
+    result_table.column("Rabat", width=50)
 
-    resultTable.heading("zDnia", text="zDnia")
-    resultTable.column("zDnia", width=80)
+    result_table.heading("cenaKoncowaEUR", text="cenaKoncowa[€]")
+    result_table.column("cenaKoncowaEUR", width=110)
 
-    resultTable['show'] = 'headings'
+    result_table.heading("zDnia", text="zDnia")
+    result_table.column("zDnia", width=80)
 
-    return resultTable
+    result_table['show'] = 'headings'
 
-def showTable(resultTable,row,column):
-    resultTable.grid(row=row, column=column)
+    return result_table
 
-def prepareSqlSelectQuery():
-    kodTowaru = str(inputField_1.get())
-    kodTowaru = kodTowaru.replace('"', '')
-    kodTowaru = kodTowaru.replace("'", "")
-    kodTowaru = kodTowaru.replace("&", "")
-    kodTowaru = kodTowaru.replace("<!--", "")
-    kodTowaru = kodTowaru.replace("-->", "")
-    kodTowaru = kodTowaru.replace(">", "")
-    kodTowaru = kodTowaru.replace("<", "")
-    kodTowaru = kodTowaru.replace("$", "")
-    kodTowaru = kodTowaru.replace("\r", "")
-    kodTowaru = kodTowaru.replace("!", "")
 
-    sql = "SELECT * FROM " + "`" + optionMenuValue.get() + "`" + " WHERE kodTowaru = '{}'".format(kodTowaru)
+def show_table(result_table, row, column):
+    result_table.grid(row=row, column=column)
+
+
+def prepare_sql_select_query():
+    kod_towaru = str(inputField_1.get())
+    kod_towaru = kod_towaru.replace('"', '')
+    kod_towaru = kod_towaru.replace("'", "")
+    kod_towaru = kod_towaru.replace("&", "")
+    kod_towaru = kod_towaru.replace("<!--", "")
+    kod_towaru = kod_towaru.replace("-->", "")
+    kod_towaru = kod_towaru.replace(">", "")
+    kod_towaru = kod_towaru.replace("<", "")
+    kod_towaru = kod_towaru.replace("$", "")
+    kod_towaru = kod_towaru.replace("\r", "")
+    kod_towaru = kod_towaru.replace("!", "")
+
+    sql = "SELECT * FROM " \
+          + \
+          "`" + optionMenuValue.get() + "`" \
+          + " WHERE kodTowaru = '{}' ORDER BY cenaKoncowa_EUR".format(kod_towaru)
     return sql
 
-def fetchDataFromDatabase():
+
+def fetch_data_from_database():
     try:
-        cursor.execute(prepareSqlSelectQuery())
+        cursor.execute(prepare_sql_select_query())
 
         results = cursor.fetchall()
     except:
@@ -77,127 +89,148 @@ def fetchDataFromDatabase():
 
     return results
 
-def insertDataIntoTable():
-    if len(fetchDataFromDatabase())>0:
+
+def insert_data_into_table():
+    if len(fetch_data_from_database()) > 0:
         try:
 
-            for row in fetchDataFromDatabase():
-                kodTowaru = row[0]
+            for row in fetch_data_from_database():
+                kod_towaru = row[0]
                 kontrahent = row[1]
-                kontrahentCennik = row[2]
-                cenaKoncowa = row[7]
-                cenaKatalogowaEUR = row[8]
-                rabat= row[10]
-                zDnia = row[11]
-                cenaKonEUR = row[9]
+                kontrahent_cennik = row[2]
+                cena_koncowa = row[7]
+                cena_katalogowa_eur = row[8]
+                rabat = row[10]
+                z_dnia = row[11]
+                cena_kon_eur = row[9]
 
-                cenaKatalogowaEUR = round(cenaKatalogowaEUR,2)
-                cenaKonEUR = round(cenaKonEUR,2)
-                rabat = round(rabat,2)
+                cena_katalogowa_eur = round(cena_katalogowa_eur, 2)
+                cena_kon_eur = round(cena_kon_eur, 2)
+                rabat = round(rabat, 2)
 
-                product= []
-                product.append(kodTowaru)
+                product = []
+                product.append(kod_towaru)
                 product.append(kontrahent)
-                product.append(kontrahentCennik)
-                product.append(cenaKoncowa)
-                product.append(cenaKatalogowaEUR)
+                product.append(kontrahent_cennik)
+                product.append(cena_koncowa)
+                product.append(cena_katalogowa_eur)
                 product.append(rabat)
-                product.append(cenaKonEUR)
-                product.append(zDnia)
+                product.append(cena_kon_eur)
+                product.append(z_dnia)
                 data.append(product)
 
-                resultTable.insert("", "end", values=(kodTowaru,kontrahent, kontrahentCennik, cenaKoncowa, cenaKatalogowaEUR, rabat, cenaKonEUR, zDnia))
+                result_table.insert("", "end", values=(
+                    kod_towaru, kontrahent, kontrahent_cennik, cena_koncowa, cena_katalogowa_eur, rabat, cena_kon_eur,
+                    z_dnia))
+
+            result_table.insert("", "end", values=(
+                '----------', '----------', '----------', '----------', '----------', '----------', '----------',
+                '----------'))
         except:
             messagebox.showinfo("Błąd POBIERANIA DANYCH")
     else:
         messagebox.showinfo("NIE ZNALEZIONO", "BRAK KODU W BAZIE")
 
-def clickSearchButton():
 
-    fetchDataFromDatabase()
-    insertDataIntoTable()
+def click_search_button():
+    fetch_data_from_database()
+    insert_data_into_table()
 
-def clickClearResults():
-    for i in resultTable.get_children():
-        resultTable.delete(i)
 
-def exportToXls(data):
+def click_clear_results():
+    for i in result_table.get_children():
+        result_table.delete(i)
 
-    workbook = xlsxwriter.Workbook('arrays.xlsx')
+
+def export_to_xls(data):
+    save_directory = filedialog.asksaveasfilename(initialdir="/",
+                                                  title="Select file",
+                                                  filetypes=(("Excel", "*.xlsx"), ("all files", "*.*")),
+                                                  defaultextension='.xlsx')
+
+    workbook = xlsxwriter.Workbook(save_directory)
     worksheet = workbook.add_worksheet()
-    worksheet.write_row(0,0,["kodTowaru","kontrahent", "cennik", "cenaKoncowa", "cenaKatalogowa_EUR", "Rabat", "cenaKoncowaEUR", "zDnia"])
-    row=0
-
+    worksheet.write_row(0, 0, ["kodTowaru", "kontrahent", "cennik", "cenaKoncowa", "cenaKatalogowa_EUR", "Rabat",
+                               "cenaKoncowaEUR", "zDnia"])
+    row = 0
 
     for col, data in enumerate(data):
         col = col + 1
         worksheet.write_row(col, row, data)
     workbook.close()
 
-def clickExport():
-    exportToXls(data)
 
-def getTablesList():
+def click_export():
+    export_to_xls(data)
+
+
+def get_tables_list():
     cursor = database.cursor()
     cursor.execute("SHOW TABLES in b2b_robocza LIKE 'zestaw%' ")
     tables = cursor.fetchall()
-    getTables_list = []
+    list_items: List[Any] = []
 
     for (table_name,) in tables:
-        getTables_list.append(table_name)
-    return getTables_list
+        list_items.append(table_name)
+    return list_items
 
-def setButton(frame,text,command,row,column):
+
+def set_button(frame, text, command, row, column):
     button = tk.Button(frame, text=text, command=command)
-    button.grid(row=row,column=column)
+    button.grid(row=row, column=column)
     return button
 
-def setLabel(frame,text,row,column):
-    label =  tk.Label(frame, text=text)
-    label.grid(row=row,column=column)
+
+def set_label(frame, text, row, column):
+    label = tk.Label(frame, text=text)
+    label.grid(row=row, column=column)
     return label
 
-def setWindow(title, width, height):
+
+def set_window(title, width, height):
     window = tk.Tk()
     window.maxsize(width=width, height=height)
     window.minsize(width=width, height=height)
     window.title(title)
     return window
 
-def setInputField(frame,row,column):
+
+def set_input_field(frame, row, column):
     field = tk.Entry(frame)
     field.grid(row=row, column=column)
     return field
 
-#main
-window_1 = setWindow('CENNIK',1000,480)
 
-database = connectToDatabase('b2b.int-technics.pl', 'b2b_roboczy', 'b2b_roboczy', 'b2b_robocza')
+# main
+window_1 = set_window('CENNIK', 1000, 480)
 
-resultTable = defineResultTable(window_1,"extended",("kodTowaru","kontrahent", "cennik", "cenaKoncowa", "cenaKatalogowa", "Rabat", "cenaKoncowaEUR", "zDnia"))
+database = connect_to_database('b2b.int-technics.pl', 'b2b_roboczy', 'b2b_roboczy', 'b2b_robocza')
 
-setResultTable(resultTable)
-showTable(resultTable,0,0)
+result_table = define_result_table(window_1, "extended", (
+    "kodTowaru", "kontrahent", "cennik", "cenaKoncowa", "cenaKatalogowa", "Rabat", "cenaKoncowaEUR", "zDnia"))
 
-searchButton = setButton(window_1,'WYSZUKAJ',clickSearchButton,5,0)
+set_result_table(result_table)
+show_table(result_table, 0, 0)
 
-clearButton = setButton(window_1,"WYCZYŚć WYSZUKIWANIE",clickClearResults,6,0)
+searchButton = set_button(window_1, 'WYSZUKAJ', click_search_button, 5, 0)
 
-exportButton = setButton(window_1,"Export do pliku",clickExport,7,0)
+clearButton = set_button(window_1, "WYCZYŚć WYSZUKIWANIE", click_clear_results, 6, 0)
 
-searchTextLabel = setLabel(window_1, 'Podaj kod Towaru: ', 1, 0)
+exportButton = set_button(window_1, "Export do pliku .xlsx", click_export, 7, 0)
 
-inputField_1 = setInputField(window_1,2,0)
+searchTextLabel = set_label(window_1, 'Podaj kod Towaru: ', 1, 0)
+
+inputField_1 = set_input_field(window_1, 2, 0)
 inputField_1.focus()
 
-scrollbar = tk.Scrollbar(window_1, command=resultTable.yview)
+scrollbar = tk.Scrollbar(window_1, command=result_table.yview)
 scrollbar.grid(row=0, column=2, sticky=tk.N)
 
 optionMenuValue = tk.StringVar(window_1)
-optionMenuValue.set(getTablesList()[0])
+optionMenuValue.set(get_tables_list()[0])
 
-tables_list = tk.OptionMenu(window_1, optionMenuValue, *getTablesList())
+tables_list = tk.OptionMenu(window_1, optionMenuValue, *get_tables_list())
 tables_list.grid(row=3, column=0, rowspan=2, sticky=tk.N)
 
 window_1.mainloop()
-disconectFromDatabase(database)
+disconect_from_database(database)
